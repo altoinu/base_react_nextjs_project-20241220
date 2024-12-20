c4049ede9d307be962c7b34780b59eca1304e391
Kaoru Kawashima <8354804+altoinu@users.noreply.github.com>
2024-12-20 17:10:43 -0500

    Initial project setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +13,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "react-hooks": eslintPluginReactHooks,
+    },
+    rules: eslintPluginReactHooks.configs.recommended.rules,
+  },
+  eslintConfigPrettier,
 ];
 
 export default eslintConfig;
