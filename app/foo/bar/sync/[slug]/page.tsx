5eea9d3a18a2dca4fe050dca510f1af01c521c27
Kaoru Kawashima <8354804+altoinu@users.noreply.github.com>
2024-12-21 21:16:01 -0500

    clean up

--- app/foo/bar/sync/[slug]/page.tsx
+++ app/foo/bar/sync/[slug]/page.tsx
@@ -1,7 +1,7 @@
-"use client";
-
 // https://nextjs.org/docs/messages/sync-dynamic-apis
 // https://nextjs.org/docs/app/building-your-application/upgrading/version-15#params--searchparams
+
+"use client";
 
 import { use } from "react";
 
@@ -11,15 +11,6 @@ type Props = {
   params: Params;
 };
 
-// https://nextjs.org/docs/app/api-reference/functions/generate-metadata
-/*
-export async function generateMetadata({ params }: Props) {
-  const { slug } = await params;
-  return { title: `Foo Bar page: ${slug}` };
-}
-*/
-
-//const SlugPage = ({ params: { slug } }: Params) => {
 const SlugPage = ({ params }: Props) => {
   const { slug } = use(params);
   return <h3>Slug: {slug}</h3>;
