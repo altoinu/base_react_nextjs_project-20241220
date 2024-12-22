30ec085563c22996244895a280ce78cc538e85b8
Kaoru Kawashima <8354804+altoinu@users.noreply.github.com>
2024-12-21 21:48:26 -0500

    simple async server component page and test

--- app/foo/bar/async/[slug]/page.tsx
+++ app/foo/bar/async/[slug]/page.tsx
@@ -1,9 +1,5 @@
-"use client";
-
 // https://nextjs.org/docs/messages/sync-dynamic-apis
 // https://nextjs.org/docs/app/building-your-application/upgrading/version-15#params--searchparams
-
-import { use } from "react";
 
 type Params = Promise<{ slug: string }>;
 
@@ -12,16 +8,13 @@ type Props = {
 };
 
 // https://nextjs.org/docs/app/api-reference/functions/generate-metadata
-/*
 export async function generateMetadata({ params }: Props) {
   const { slug } = await params;
   return { title: `Foo Bar page: ${slug}` };
 }
-*/
 
-//const SlugPage = ({ params: { slug } }: Params) => {
-const SlugPage = ({ params }: Props) => {
-  const { slug } = use(params);
+const SlugPage = async ({ params }: Props) => {
+  const { slug } = await params;
   return <h3>Slug: {slug}</h3>;
 };
 
