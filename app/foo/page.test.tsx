b7d3a8f5930f173620237345ef71ce61b24959bf
Kaoru Kawashima <8354804+altoinu@users.noreply.github.com>
2024-12-23 07:21:02 -0500

    sample custom component

--- app/foo/page.test.tsx
+++ app/foo/page.test.tsx
@@ -1,10 +1,13 @@
+import HelloBoxComponent from "../_components/HelloBoxComponent";
 import Page from "./page";
 import { render, screen } from "@testing-library/react";
 import { usePathname } from "next/navigation";
 
 jest.mock("next/navigation");
-
 const mockUsePathname = usePathname as jest.Mock;
+
+//<h2>This is mocked HelloBoxComponent.</h2>
+jest.mock("../_components/HelloBoxComponent");
 
 describe("foo page", () => {
   beforeAll(() => {
@@ -19,7 +22,24 @@ describe("foo page", () => {
 
   it("should render a heading", () => {
     render(<Page />);
+
     const heading = screen.getByRole("heading");
     expect(heading.textContent).toBe("This is Foo page.");
+  });
+
+  it("should render HelloBoxComponents", () => {
+    render(<Page />);
+
+    expect(HelloBoxComponent).toHaveBeenCalledTimes(2);
+    expect(HelloBoxComponent).toHaveBeenNthCalledWith(
+      1,
+      expect.objectContaining({ personName: "Kaoru" }),
+      undefined,
+    );
+    expect(HelloBoxComponent).toHaveBeenNthCalledWith(
+      2,
+      expect.objectContaining({ personName: "John", secondPersonName: "Doe" }),
+      undefined,
+    );
   });
 });
