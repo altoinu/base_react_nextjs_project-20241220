542aa9766c33a3dfd8d37b6bdd02902ad4357c4c
Kaoru Kawashima <8354804+altoinu@users.noreply.github.com>
2025-03-25 17:31:09 -0400

    notes, some regex update

--- app/foo/page.test.tsx
+++ app/foo/page.test.tsx
@@ -4,7 +4,12 @@ import { describe, expect, it } from "@jest/globals";
 import { render, screen } from "@testing-library/react";
 import { usePathname } from "next/navigation";
 
-// mock a hook
+// Mock a hook
+/*
+xxx as jest.Mock works with global jest with @types/jest,
+but not with @jest/globals for some reason... so for now it is not
+explicitly imported above
+*/
 jest.mock("next/navigation");
 const mockUsePathname = usePathname as jest.Mock;
 mockUsePathname.mockReturnValue("/mockURL");
